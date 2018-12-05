aeae5b4a9ed07a11d1705c70b59c451769e4e1f5
felloz <serranol82@gmail.com>
2018-12-05 08:59:13 -0400

    En este codigo logre retirar solo los errores con codigo 550

--- archivos.pl
+++ archivos.pl
@@ -8,7 +8,15 @@ use strict;
 use warnings;
 
 my $resultado;
-
+my $smtpDiagnosticCode;
+my $smtpDiagnosticCode1;
+my %smtpDiagnosticCode1;
+my %devueltos;
+my %row;
+my $i;
+my %smtpCode;
+my $smtpCode;
+my $row;
 #my $archivo = '/home/lserrano/Documentos/perl/files/devueltos2018ls.txt';
 #open (my $file, '<:encoding(UTF-8)', $archivo)
 #  or die "No se pudo abrir el archivo '$archivo' $!";
@@ -19,10 +27,17 @@ my $resultado;
 #  }
 
 open (BUSCAR, "<", "/home/lserrano/Documentos/perl/files/devueltos2018ls.txt");
-
+#print <BUSCAR>;
 while (<BUSCAR>){
-  if(m/([1-9])\1/){
-    $resultado = $_;
-    print $resultado;
+  #$smtpDiagnosticCode = undef;
+  #(m/([1-9])\1/)
+  if($_ =~ (m/([\d{3}]\s\d[.]\d[.]\d)/)){
+   #if ($_ =~ /^(\d{3}\s\d{1}\.\d{1}\.\d{1})/ig){
+   $smtpDiagnosticCode = $_;
+   $smtpDiagnosticCode1 = substr($smtpDiagnosticCode, 5,10);
+   #print $smtpDiagnosticCode1;
+   if ($smtpDiagnosticCode1 =~ /550\s\d\.\d\.\d$/){
+      print $smtpDiagnosticCode1 . "\n";
+   }
   };
 }
